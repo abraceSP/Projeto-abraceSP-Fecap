e9fead018248e5630f534e105c8a9720b2e1498e
pedro-lemos <pedroefernando38@gmail.com>
2024-11-18 20:11:10 -0300

    fix: arruma requisicoes backend

--- src/backend/server.js
+++ src/backend/server.js
@@ -12,13 +12,12 @@ require("dotenv").config(); // Carregar variáveis de ambiente
 const app = express();
 
 // Configuração de CORS para permitir requisições do frontend em localhost
-// app.use(cors({
-//   origin: process.env.CORS_ORIGIN || 'https://abracesp.netlify.app',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   credentials: true,
-// }));
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+}));
 
-app.use(cors());
 app.use(express.json());
 
 // app.options('*', cors({
@@ -126,7 +125,7 @@ const authenticateToken = (req, res, next) => {
 app.get("/admin", authenticateToken, (req, res) => {
   const { id } = req.usuario;
   db.query(
-    "SELECT nome, foto FROM usuariosAdmin WHERE idAdmin = ?",
+    "SELECT * FROM usuariosAdmin WHERE idAdmin = ?",
     [id],
     (err, results) => {
       if (err) {
@@ -270,7 +269,7 @@ app.put("/cadastros/:idUsuario", (req, res) => {
 app.get("/admin", authenticateToken, (req, res) => {
   const { id } = req.usuario;
   db.query(
-    "SELECT nome, foto FROM usuariosAdmin WHERE idAdmin = ?",
+    "SELECT * FROM usuariosAdmin WHERE idAdmin = ?",
     [id],
     (err, results) => {
       if (err) {
@@ -286,26 +285,6 @@ app.get("/admin", authenticateToken, (req, res) => {
   );
 });
 
-// Endpoint para atualizar dados do admin logado
-app.put("/admin", authenticateToken, upload.single("foto"), (req, res) => {
-  const { id } = req.usuario;
-  const { nome } = req.body;
-  const foto = req.file ? `public/uploads/${req.file.filename}` : null;
-
-  const updateQuery = `
-    UPDATE usuariosAdmin
-    SET nome = ?, foto = ?
-    WHERE idAdmin = ?
-  `;
-
-  db.query(updateQuery, [nome, foto, id], (err) => {
-    if (err) {
-      console.error("Erro ao atualizar dados do admin: ", err);
-      return res.status(500).send("Erro ao atualizar dados do admin");
-    }
-    res.json({ nome, foto });
-  });
-});
 
 // Endpoint para obter todos os prestadores de serviço
 
